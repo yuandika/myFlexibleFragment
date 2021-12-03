cfec74da5539d0e18e730961f836dd6d692580e5
Yuandika Alfahreiza <yuandika11@gmail.com>
2021-12-03 18:47:58 +0700

    memanggil activity dari fragment

--- app/src/main/java/com/dicoding/myflexiblefragment/DetailCategoryFragment.kt
+++ app/src/main/java/com/dicoding/myflexiblefragment/DetailCategoryFragment.kt
@@ -1,5 +1,6 @@
 package com.dicoding.myflexiblefragment
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -46,6 +47,11 @@ class DetailCategoryFragment : Fragment(){
             val mOptionDialogFragment = OptionDialogFragment()
             val mFragmentManager = childFragmentManager
             mOptionDialogFragment.show(mFragmentManager, OptionDialogFragment::class.java.simpleName)
+        }
+
+        btnProfile.setOnClickListener {
+            val mIntent = Intent(activity, ProfileActivity::class.java)
+            startActivity(mIntent)
         }
 
         if (savedInstanceState != null) {
